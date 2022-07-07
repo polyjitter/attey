7328176be5da1129e96468914b7a478888156b3b
Adriene Kae <polyjitter@fastmail.com>
2022-07-07 00:10:38 -0400

    Oh my god it works

--- attey/__init__.py
+++ attey/__init__.py
@@ -1,17 +1,21 @@
-import json
-
 import rethinkdb
+import toml
 
-with open("./config.json") as f:
-    _config = json.load(f)
+with open("./pyproject.toml") as f:
+    _poetry = toml.load(f)
 
-    __version__ = _config["VERSION"]
+    __version__ = _poetry["tool"]["poetry"]["version"]
+
+with open("./config.toml") as f:
+    _config = toml.load(f)
+
     # __authors__ = _config["AUTHORS"]
 
-    MAINTENANCE = _config["MAINTENANCE"]
+    TOKEN = _config["bot"]["token"]
+    MAINTENANCE = _config["bot"]["maintenance"]
 
-    if _config['RETHINK']['NAME']:
+    if _config["rethink"]["name"]:
         re = rethinkdb.RethinkDB()
         re.set_loop_type("asyncio")
-        DB_NAME = _config['RETHINK']['NAME']
-        DB_TABLES = _config['RETHINK']['TABLES']
+        DB_NAME = _config["rethink"]["name"]
+        DB_TABLES = _config["rethink"]["tables"]
--- attey/__main__.py
+++ attey/__main__.py
@@ -1,25 +1,22 @@
 import os
-import json
 
 import hikari
 import lightbulb
+import toml
 
 import attey
 
 
 def create_bot() -> lightbulb.BotApp:
 
-    with json.load(open("./config.json")) as f:
-        token = f["TOKEN"]
-
     bot = lightbulb.BotApp(
-        token=token,
-        intents=[
-            hikari.Intents.GUILD_MESSAGES,
-            hikari.Intents.GUILD_MESSAGE_REACTIONS,
-            hikari.Intents.MESSAGE_CONTENT,
-            hikari.Intents.GUILD_WEBHOOKS,
-        ],
+        token=attey.TOKEN,
+        intents=(
+            hikari.Intents.GUILD_MESSAGES
+            | hikari.Intents.GUILD_MESSAGE_REACTIONS
+            | hikari.Intents.MESSAGE_CONTENT
+            | hikari.Intents.GUILD_WEBHOOKS
+        ),
     )
 
     bot.load_extensions_from("./attey/commands")
